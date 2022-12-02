34dfa4e0612fe158b34f8ec118b6a66d748f8b0e
https://github.com/Wasi752 <osiur2@gmail.com>
2022-12-03 01:54:02 +0600

    JSON Database

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const fs = require("fs")
+const { parse } = require('path')
 
 app.get('/', (req, res) => {
     res.send('Bismillahir Rahmanir Rahim')
 })
-app.get('/start', (req, res) => {
-    fs.writeFile('Count', '0', () => { })
-    res.send('You have writen')
-})
-app.get('/num', (req, res) => {
-    fs.readFile("Count", 'utf8', (err, data) => {
-        const x = parseInt(data) + 1
-        const text = x.toString()
-        fs.writeFile('Count', text, () => { })
-        res.send("Count increased")
+app.get('/employees', (req, res) => {
+    fs.readFile("database", 'utf8', (err, data) => {
+        const allData = JSON.parse(data) 
+        res.send(JSON.stringify(allData.employees))
     })
 })
-
-
+app.get('/create-employee/:name', (req, res) => {
+    fs.readFile("database", 'utf8', (err, data) => {
+        const allData = JSON.parse(data)
+        allData.employees.push(req.params.name) 
+        fs.writeFile('database', JSON.stringify(allData), () => { })
+        res.send(`${req.params.name}`)
+    })
+})
+app.get('/departments', (req, res) => {
+    fs.readFile("database", 'utf8', (err, data) => {
+        const allData = JSON.parse(data) 
+        res.send(JSON.stringify(allData.departments))
+    })
+})
+app.get('/create-department/:name', (req, res) => {
+    fs.readFile("database", 'utf8', (err, data) => {
+        const allData = JSON.parse(data)
+        allData.departments.push(req.params.name) 
+        fs.writeFile('database', JSON.stringify(allData), () => { })
+        res.send(`${req.params.name}`)
+    })
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
