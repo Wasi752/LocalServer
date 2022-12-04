8106a223535fd8b67cc22c3463a909e508a222fa
https://github.com/Wasi752 <osiur2@gmail.com>
2022-12-04 22:55:00 +0600

    Create API by Postman

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3001
 const fs = require("fs")
 const { parse } = require('path')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Bismillahir Rahmanir Rahim')
@@ -13,26 +16,30 @@ app.get('/employees', (req, res) => {
         res.send(JSON.stringify(allData.employees))
     })
 })
-app.get('/create-employee/:name', (req, res) => {
+app.post('/create-employee', (req, res) => {
     fs.readFile("database", 'utf8', (err, data) => {
         const allData = JSON.parse(data)
-        allData.employees.push(req.params.name) 
+        const employeeData = req.body;
+        employeeData.id = allData.employees.length + 1;
+        allData.employees.push(employeeData) 
         fs.writeFile('database', JSON.stringify(allData), () => { })
-        res.send(`${req.params.name}`)
+        res.send(`${req.body.name}`)
     })
 })
 app.get('/departments', (req, res) => {
     fs.readFile("database", 'utf8', (err, data) => {
-        const allData = JSON.parse(data) 
+        const allData = JSON.parse(data)
         res.send(JSON.stringify(allData.departments))
     })
 })
-app.get('/create-department/:name', (req, res) => {
+app.post('/create-department', (req, res) => {
     fs.readFile("database", 'utf8', (err, data) => {
         const allData = JSON.parse(data)
-        allData.departments.push(req.params.name) 
+        const departmentData = req.body;
+        departmentData.id = allData.departments.length + 1; 
+        allData.departments.push(departmentData) 
         fs.writeFile('database', JSON.stringify(allData), () => { })
-        res.send(`${req.params.name}`)
+        res.send(`${req.body.name}`)
     })
 })
 app.listen(port, () => {
