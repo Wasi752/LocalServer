60016bee733f259f0f50099a612b09e801d6ecd5
https://github.com/Wasi752 <osiur2@gmail.com>
2022-12-10 11:46:55 +0600

    Replace Object Property2

--- index.js
+++ index.js
@@ -63,13 +63,15 @@ app.post('/create-student', (req, res) => {
         res.send(`${req.body.name}`)
     })
 })
-app.get('/students/:id', (req, res) => {
+app.put('/students/:id', (req, res) => {
     fs.readFile("employeeDatabase", 'utf8', (err, data) => {
         const allData = JSON.parse(data) 
-        const studentInfoByID = allData.students.find(x => x.id == req.params.id); 
-        console.log(studentInfoByID);
+        const studentInfoByID = allData.students.find(x => x.id == req.params.id);
+        studentInfoByID.name_arabic = req.body.name_arabic;
+        allData.students.push(studentInfoByID) 
+        fs.writeFile("employeeDatabase", JSON.stringify(allData), () => { })
         res.send(JSON.stringify(studentInfoByID))
-    })
+    }) 
 })
 
 app.get('/registared-students', (req, res) => {
