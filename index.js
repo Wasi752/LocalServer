e9c776f1d8aad0f57bfe8e742810f978b38b60ce
https://github.com/Wasi752 <osiur2@gmail.com>
2022-12-15 02:10:25 +0600

    Data Replace

--- index.js
+++ index.js
@@ -100,7 +100,6 @@ app.put('/students/:id', (req, res) => {
         const allData = JSON.parse(data) 
         const studentInfoByID = allData.students.find(x => x.id == req.params.id);
         studentInfoByID.name_arabic = req.body.name_arabic;
-        allData.students.push(studentInfoByID) 
         fs.writeFile("employeeDatabase", JSON.stringify(allData), () => { })
         res.send(JSON.stringify(studentInfoByID))
     }) 
