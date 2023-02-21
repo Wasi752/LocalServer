58aa3a055d8cd336f8a020d70667ff8621a33398
wasi752 <osiur2@gmail.com>
2023-02-21 13:58:59 +0600

    Validation DRY

--- allFunction.js
+++ allFunction.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const { body, check, validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const allFunctions = (app) => {
     const modify = (dbName, callBack, res) => fs.readFile('databases/' + dbName, 'utf8', (err, data) => {
@@ -19,30 +19,50 @@ const allFunctions = (app) => {
         }, res);
     });
 
-    const POST = (rurl, dbName, prop, arr) => app.post(rurl, (req, res) => {
-        modify(dbName, (allData) => {
-            const postData = req.body;
-            postData.id = allData[prop].length + 1;
-            allData[prop].push(postData)
-            writeFile(dbName, allData)
-            return postData;
-        }, res)
-    })
-    const PUT = (rurl, dbName, prop, arr) => app.put(rurl, (req, res) => {
-        modify(dbName, (allData) => {
-            const dataByID = allData[prop].filter(x => x.id == req.params.id)[0];
-            arr.forEach((prop) => dataByID[prop] = req.body[prop]);
-            writeFile(dbName, allData);
-            return dataByID;
-        }, res)
-    })
+    const POST = (rurl, dbName, prop, arr) => app.post(rurl,
+        arr.map(x => x[1]),
+        (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            modify(dbName, (allData) => {
+                const postData = req.body;
+                postData.id = allData[prop].length + 1;
+                allData[prop].push(postData)
+                writeFile(dbName, allData)
+                return postData;
+            }, res)
+        })
+    const PUT = (rurl, dbName, prop, arr) => app.put(rurl,
+        arr.map(x => x[1]),
+        (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            modify(dbName, (allData) => {
+                const dataByID = allData[prop].filter(x => x.id == req.params.id)[0];
+                arr.map(x => x[0]).forEach((prop) => dataByID[prop] = req.body[prop]);
+                writeFile(dbName, allData);
+                return dataByID;
+            }, res)
+        })
     const DELETE = (rurl, dbname, prop) => app.delete(rurl, (req, res) => {
         modify(dbname, (allData) => {
             allData[prop] = allData[prop].filter(x => x.id != req.params.id);
             writeFile(dbname, allData);
         }, res)
-    })
+    });
+
+    const createCRUD = (baseRoute, dbName, tableName, properties) => {
+        GET(baseRoute, dbName, tableName);
+        GETID(baseRoute + '/:id', dbName, tableName);
+        POST(baseRoute, dbName, tableName, properties);
+        PUT(baseRoute + '/:id', dbName, tableName, properties);
+        DELETE(baseRoute + '/:id', dbName, tableName)
+    }
     const writeFile = (dbName, data) => fs.writeFile(dbName, JSON.stringify(data), () => { });
-    return { writeFile, GET, GETID, POST, PUT, DELETE }
+    return { writeFile, createCRUD }
 }
 module.exports = allFunctions;
--- router.js
+++ router.js
@@ -1,92 +1,84 @@
+const { check } = require("express-validator");
 const fs = require("fs")
 const router = require('express').Router();
 
-const { POST, PUT, DELETE, GET, GETID, writeFile } = require('./allFunction')(router)
+const { createCRUD, writeFile } = require('./allFunction')(router)
 
 router.get('/', (req, res) => {
     res.send('Bismillahir Rahmanir Rahim')
 })
-const createCRUD = (baseRoute, dbName, tableName, properties) => {
-    GET(baseRoute, dbName, tableName);
-    GETID(baseRoute + '/:id', dbName, tableName);
-    POST(baseRoute, dbName, tableName, properties);
-    PUT(baseRoute + '/:id', dbName, tableName, properties);
-    DELETE(baseRoute + '/:id', dbName, tableName)
-}
-
-//---------------
 createCRUD('/employees', 'employeeDatabase.json', 'employees', [
-    'name',
-    'father',
-    'mother',
-    'present_address',
-    'permanent_address',
-    'academic_achievement',
-    'languages_skills',
-    'designation',
-    'contact_no',
-    'e_mail',
-    'room_no',
-    'image',
-    'nationality',
-    'brith',
-    'nid'
+    ['name', check('name').exists().withMessage('name must be provided').isLength({min : 1, max : 20}).withMessage('name must be between 1 to 20 chars')],
+    ['father', check('father').exists().withMessage('father must be provided').isLength({min : 1, max : 20}).withMessage('father must be between 1 to 20 chars')],
+    ['mother', check('mother').exists().withMessage('mother must be provided').isLength({min : 1, max : 20}).withMessage('mother must be between 1 to 20 chars')],
+    ['present_address', check('present_address').exists().withMessage('present_address must be provided').isLength({min : 1, max : 20}).withMessage('present_address must be between 1 to 20 chars')],
+    ['permanent_address', check('permanent_address').exists().withMessage('permanent_address must be provided').isLength({min : 1, max : 20}).withMessage('permanent_address must be between 1 to 20 chars')],
+    ['academic_achievement', check('academic_achievement').exists().withMessage('academic_achievement must be provided').isLength({min : 1, max : 20}).withMessage('academic_achievement must be between 1 to 20 chars')],
+    ['languages_skills', check('languages_skills').exists().withMessage('languages_skills must be provided').isLength({min : 1, max : 20}).withMessage('languages_skills must be between 1 to 20 chars')],
+    ['designation', check('designation').exists().withMessage('designation must be provided').isLength({min : 1, max : 20}).withMessage('designation must be between 1 to 20 chars')],
+    ['contact_no', check('contact_no').exists().withMessage('contact_no must be provided').isLength({min : 1, max : 20}).withMessage('contact_no must be between 1 to 20 chars')],
+    ['e_mail', check('e_mail').exists().withMessage('e_mail must be provided').isLength({min : 1, max : 20}).withMessage('e_mail must be between 1 to 20 chars')],
+    ['room_no', check('room_no').exists().withMessage('room_no must be provided').isLength({min : 1, max : 20}).withMessage('room_no must be between 1 to 20 chars')],
+    ['image', check('image').exists().withMessage('image must be provided').isLength({min : 1, max : 20}).withMessage('image must be between 1 to 20 chars')],
+    ['nationality', check('nationality').exists().withMessage('nationality must be provided').isLength({min : 1, max : 20}).withMessage('nationality must be between 1 to 20 chars')],
+    ['brith', check('brith').exists().withMessage('brith must be provided').isLength({min : 1, max : 20}).withMessage('brith must be between 1 to 20 chars')],
+    ['nid', check('nid').exists().withMessage('nid must be provided').isLength({min : 1, max : 20}).withMessage('nid must be between 1 to 20 chars')],
 ]);
-createCRUD('/boards', 'employeeDatabase.json', 'boards', ['name_arabic', 'name_bangala', 'name_english', 'address']);
-createCRUD('/fazilatResult', 'madrasaResult.json', 'fazilatResult', ['madrasa', 'name', 'fname']);
-createCRUD('/result', 'madrasaResult.json', 'results', ['mname', 'name']);
-createCRUD('/madrasa', 'registration.json', 'madrasas', ['name_arabic', 'name_bangala', 'name_english', 'muhtamim']);
-createCRUD('/studentRegistration', 'registration.json', 'students', ['name', 'name_arabic', 'name_english', 'father', 'brith'])
-createCRUD('/users', 'madrasa.json', 'users', ['name', 'password', 'mobile']);
+// createCRUD('/boards', 'employeeDatabase.json', 'boards', ['name_arabic', 'name_bangala', 'name_english', 'address']);
+// createCRUD('/fazilatResult', 'madrasaResult.json', 'fazilatResult', ['madrasa', 'name', 'fname']);
+// createCRUD('/result', 'madrasaResult.json', 'results', ['mname', 'name']);
+// createCRUD('/madrasa', 'registration.json', 'madrasas', ['name_arabic', 'name_bangala', 'name_english', 'muhtamim']);
+// createCRUD('/studentRegistration', 'registration.json', 'students', ['name', 'name_arabic', 'name_english', 'father', 'brith'])
+// createCRUD('/users', 'madrasa.json', 'users', ['name', 'password', 'mobile']);
 // ======== ---------------------------- ===========
 
-router.post('/studentRegistration', (req, res) => {
-    fs.readFile('registration.json', "utf8", (err, data) => {
-        const allData = JSON.parse(data)
-        const reqData = req.body;
-        const getError = () => {
-            const uppercaseRegExp = /(?=.*?[A-Z])/;
-            const lowercaseRegExp = /(?=.*?[a-z])/;
-            const digitsRegExp = /(?=.*?[0-9])/;
-            const specialCharRegExp = /(?=.*?[#?!@$%^&*-])/;
-            const minLengthRegExp = /.{8,}/;
-            const uppercasePassword = uppercaseRegExp.test(req.body.password);
-            const lowercasePassword = lowercaseRegExp.test(req.body.password);
-            const digitsPassword = digitsRegExp.test(req.body.password);
-            const specialCharPassword = specialCharRegExp.test(req.body.password);
-            const minLengthPassword = minLengthRegExp.test(req.body.password);
-            const validEmailAddress = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            const validEmail = validEmailAddress.test(req.body.email);
-            const validMobileNumber = /^[0-9]*$/;
-            const validNumber = validMobileNumber.test(req.body.mobileNumber);
-            IFELSE(req.body.name.length < 5, 'Name must be contain atleast 5 Characters')
-            IFELSE('', '', `${req.body.password}.length === 0`, 'Password is empty');
-            IFELSE('', '', !uppercasePassword, 'At least one Uppercase');
-            IFELSE('', '', !lowercasePassword, 'At least one Lowercase');
-            IFELSE('', '', !digitsPassword, 'At least one digit');
-            IFELSE('', '', !specialCharPassword, 'At least one Special Characters');
-            IFELSE('', '', !minLengthPassword, 'At least minumum 8 characters');
-            IFELSE('', '', req.body.confirmPassword !== req.body.password, 'Confirm password is not matched');
-            IFELSE('', '', `${req.body.email}.length < 10`, 'E-mail is not valid');
-            IFELSE('', '', !validEmail, 'Email Addre must be in valid formate with @ symbol')
-            IFELSE('', '', req.body.mobileNumber < 11, 'Mobile number must be 11 digit with in valid formate');
-            IFELSE('', '', !validNumber, 'Mobile Number not valid');
-            return "";
-        }
-        const error = getError();
-        if (error !== "") {
-            return res.status(400).send(JSON.stringify({
-                error: error
-            }))
-        }
-        const rawImageString = reqData.image.replace(/^data:image\/jpeg;base64,/, "");
-        const buffer = Buffer.from(rawImageString, "base64");
-        reqData.id = allData.students.length + 1;
-        fs.writeFile(`public/student/${reqData.id}.jpeg`, buffer, () => { });
-        reqData.image = `${reqData.id}.jpeg`;
-        allData.students.push(reqData);
-        writeFile('registration.json', allData)
-        res.send(JSON.stringify(reqData));
-    });
-});
+// router.post('/studentRegistration', (req, res) => {
+//     fs.readFile('registration.json', "utf8", (err, data) => {
+//         const allData = JSON.parse(data)
+//         const reqData = req.body;
+//         const getError = () => {
+//             const uppercaseRegExp = /(?=.*?[A-Z])/;
+//             const lowercaseRegExp = /(?=.*?[a-z])/;
+//             const digitsRegExp = /(?=.*?[0-9])/;
+//             const specialCharRegExp = /(?=.*?[#?!@$%^&*-])/;
+//             const minLengthRegExp = /.{8,}/;
+//             const uppercasePassword = uppercaseRegExp.test(req.body.password);
+//             const lowercasePassword = lowercaseRegExp.test(req.body.password);
+//             const digitsPassword = digitsRegExp.test(req.body.password);
+//             const specialCharPassword = specialCharRegExp.test(req.body.password);
+//             const minLengthPassword = minLengthRegExp.test(req.body.password);
+//             const validEmailAddress = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//             const validEmail = validEmailAddress.test(req.body.email);
+//             const validMobileNumber = /^[0-9]*$/;
+//             const validNumber = validMobileNumber.test(req.body.mobileNumber);
+//             IFELSE(req.body.name.length < 5, 'Name must be contain atleast 5 Characters')
+//             IFELSE('', '', `${req.body.password}.length === 0`, 'Password is empty');
+//             IFELSE('', '', !uppercasePassword, 'At least one Uppercase');
+//             IFELSE('', '', !lowercasePassword, 'At least one Lowercase');
+//             IFELSE('', '', !digitsPassword, 'At least one digit');
+//             IFELSE('', '', !specialCharPassword, 'At least one Special Characters');
+//             IFELSE('', '', !minLengthPassword, 'At least minumum 8 characters');
+//             IFELSE('', '', req.body.confirmPassword !== req.body.password, 'Confirm password is not matched');
+//             IFELSE('', '', `${req.body.email}.length < 10`, 'E-mail is not valid');
+//             IFELSE('', '', !validEmail, 'Email Addre must be in valid formate with @ symbol')
+//             IFELSE('', '', req.body.mobileNumber < 11, 'Mobile number must be 11 digit with in valid formate');
+//             IFELSE('', '', !validNumber, 'Mobile Number not valid');
+//             return "";
+//         }
+//         const error = getError();
+//         if (error !== "") {
+//             return res.status(400).send(JSON.stringify({
+//                 error: error
+//             }))
+//         }
+//         const rawImageString = reqData.image.replace(/^data:image\/jpeg;base64,/, "");
+//         const buffer = Buffer.from(rawImageString, "base64");
+//         reqData.id = allData.students.length + 1;
+//         fs.writeFile(`public/student/${reqData.id}.jpeg`, buffer, () => { });
+//         reqData.image = `${reqData.id}.jpeg`;
+//         allData.students.push(reqData);
+//         writeFile('registration.json', allData)
+//         res.send(JSON.stringify(reqData));
+//     });
+// });
 module.exports = router;
